db9c85aef7065311a0c5ae3b33fca02fdfa762d1
efrontinan <elenauxui@gmail.com>
2025-01-03 15:40:51 +0100

    Challenge 19 solved with errors

--- Reto19.js
+++ Reto19.js
@@ -1,5 +1,9 @@
 function distributeWeight(weight) {
 
+    let remaining = weight
+
+    const boxes = []
+
     const boxRepresentations = {
         1: [" _ ", "|_|"],
         2: [" ___ ", "|___|"],
@@ -7,8 +11,35 @@ function distributeWeight(weight) {
         10: [" _________ ", "|         |", "|_________|"]
     }
 
+    const numbers = Object.keys(boxRepresentations).map(Number).reverse()
 
-    return '';
+    findBiggestBox = (boxWeight) => {
+        return numbers.find(number => boxWeight >= number)
+    }
+
+    while (remaining > 0) {
+        const box = findBiggestBox(remaining)
+        boxes.push(boxRepresentations[box])
+        remaining -= box
+    }
+
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i + 1]) {
+            let lastBottom = boxes[i + 1][boxes[i + 1].length - 1]
+            const addBottom = boxes[i][0]?.slice(0, lastBottom.length + 1)
+
+            lastBottom = `${lastBottom?.trim()}${addBottom?.trim()}`
+
+            boxes[i + 1][boxes[i + 1].length - 1] = lastBottom
+            boxes[i].shift()
+
+        }
+    }
+
+    const result = boxes.map(eachBox => eachBox.join('\n'))
+
+
+    return result.reverse().join('\n')
 }
 
-console.log(distributeWeight(2))
+console.log(distributeWeight(6))
